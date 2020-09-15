0b839e3844378def5ba015e6ad61910be24ae965
Roman <61251937+RomanKlasnyy@users.noreply.github.com>
2020-09-15 10:34:39 +0300

    Update task1.2.py

--- part 1/task1.2.py
+++ part 1/task1.2.py
@@ -6,7 +6,7 @@ import re
 phrase = '''
 I told him to visit https://regexr.com/, 
 but he decided to check www.fakeregexr.com/ 
-or even http://veryfakeregexr.com/ OMG, it is no unsecure UwU
+or even http://veryfakeregexr.com/ OMG, it is so unsecure UwU
 '''
 
 result = re.findall(r'(w{3}.\w+\.\w+)', phrase)          # triggers on www. only
